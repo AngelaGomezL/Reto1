5a80b243792f44cb57a64803cd067ffa2cdb9c1f
AngelaGomezL <angela.gomezl@udea.edu.co>
2023-02-24 11:58:55 -0500

    prueba

--- Usuario-service/src/main/java/com/reto/comidas/domain/modelo/Rol.java
+++ Usuario-service/src/main/java/com/reto/comidas/domain/modelo/Rol.java
@@ -2,12 +2,11 @@ package com.reto.comidas.domain.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
 @Setter
 @Getter
 public class Rol {
     private Integer id;
+
     private String nombre;
     private String descripcion;
 }

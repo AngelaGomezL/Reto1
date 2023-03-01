6dc554c80c00fd8af962051ebcbbf816783748d0
AngG12 <98767870+AngelaGomezL@users.noreply.github.com>
2023-03-01 13:09:43 -0500

    Delete Plazoleta-service directory

--- Plazoleta-service/src/main/java/com/example/Plazoleta/insfrastructure/output/exception/RestauranteAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Plazoleta.insfrastructure.output.exception;
-
-public class RestauranteAlreadyExistException {
-    public RestauranteAlreadyExistException(){
-        super();
-    }
-}

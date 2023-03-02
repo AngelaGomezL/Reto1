0e54e587c4f65d2874eeedb782d505caff8aac5f
AngelaGomezL <angela.gomezl@udea.edu.co>
2023-03-02 12:21:02 -0500

    prueba

--- Plazoleta-service/src/main/java/com/example/Plazoleta/application/dto/Restaurante/RestauranteRequest.java
+++ Plazoleta-service/src/main/java/com/example/Plazoleta/application/dto/Restaurante/RestauranteRequest.java
@@ -2,17 +2,23 @@ package com.example.Plazoleta.application.dto.Restaurante;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class RestauranteRequest {
 
-    private Integer id;
+    @Pattern(regexp = "^[a-zA-Z\\s]*$")
     private String nombre;
     private String direccion;
     private Integer idPropietario;
+    @Pattern(regexp = "^(\\+57)?3\\d{9}$")
     private String telefono;
+    @URL(message = "must be a url")
     private String urlLogo;
-    //private Integer idRestaurante;
+    @Pattern(regexp = "^[0-9]*$")
     private String nit;
+
 }
--- Usuario-service/src/main/java/com/reto/comidas/application/dto/RestauranteRequest.java
+++ Usuario-service/src/main/java/com/reto/comidas/application/dto/RestauranteRequest.java
@@ -2,16 +2,23 @@ package com.reto.comidas.application.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class RestauranteRequest {
     //private Integer id;
-    private String name;
+    @Pattern(regexp = "^[a-zA-Z\\s]*$")
+    private String nombre;
     private String direccion;
     private Integer idPropietario;
+    @Pattern(regexp = "^(\\+57)?3\\d{9}$")
     private String telefono;
+    @URL(message = "must be a url")
     private String urlLogo;
+    @Pattern(regexp = "^[0-9]*$")
     private String nit;
 
 }
